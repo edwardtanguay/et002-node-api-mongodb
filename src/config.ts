9dbc8a99379e75bf997141f69e955064f1e6664d
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-11 10:22:40 +0100

    logger out

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,7 @@
-export const port = 3610;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// const { PORT = 3000 } = process.env;
+export const port = process.env.PORT || 3000;
 export const logPathAndFileName = './src/logs/log.txt';
